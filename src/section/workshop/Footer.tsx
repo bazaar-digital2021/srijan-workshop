d1c282d0ffa7eca4f3558bc97d1b343d18905a47
deadeye03 <yakshitkr@gmail.com>
2025-05-15 12:27:32 +0530

    hide home page

--- src/section/workshop/Footer.tsx
+++ src/section/workshop/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-amber-200 text-black py-8 md:py-16">
             <div className="container mx-auto max-w-5xl flex flex-col gap-6">
-                <p>
+                <p className="px-4 md:px-0">
                     NOTE: This website in no way is affiliated with Facebook™ or any Facebook™ entities. Once you leave Facebook™ the responsibility no longer is on their site. We make every attempt to clearly state and show all proof. We do not sell your email or any information. Can-Spam compliant.
                 </p>
                 <hr className="bg-black " />
